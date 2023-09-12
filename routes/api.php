5f7fd75ffcef24e430af8b35e41d9e029b24b32e
Skyfe24 <fabio.erase@gmail.com>
2023-09-12 17:52:40 +0200

    Milestone 1 and 2 completed

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +15,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/test', function () {
+    $names = ['Fabio', 'Flavio', 'Frizz'];
+    $total = count($names);
+    return response()->json(compact('names', 'total'));
+});
+
+Route::get('/projects', function () {
+    $projects = Project::all();
+    return response()->json($projects);
+});
+
+Route::resource('projects', ProjectController::class);
